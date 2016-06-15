08ee80f4134d70c6d99413b35bf6b0f12d34cea0
Csaba Okrona <ochronus@ochronus.com>
2016-06-15 11:28:01 +0200

    More accurate location + address printed

--- drunkass_compass/drunkass_compass.py
+++ drunkass_compass/drunkass_compass.py
@@ -1,25 +1,25 @@
-
 import googlemaps
 import keyring
 import json
 import requests
-from datetime import datetime
 import commands
-from bs4 import BeautifulSoup
 
 
 def main():
     # get API key for google maps
-    api_key = keyring.get_password('drunkass-compass-api-key', 'drunkass-compass-api-key')
+    api_key = keyring.get_password(
+        'drunkass-compass-api-key', 'drunkass-compass-api-key')
     if api_key is None:
         print 'Missing google maps api key'
         exit(1)
 
     # get current location
-    (status, output) = commands.getstatusoutput("CoreLocationCLI -once YES -format '%latitude %longitude'")
+    (status, output) = commands.getstatusoutput(
+        "CoreLocationCLI -once YES -format '%latitude %longitude'")
     if status != 0:
-        print "Could not run CoreLocationCLI, using freegeoip - this is not accurate"    
-        response_json = json.loads(requests.get('http://freegeoip.net/json/').content)
+        print "Could not run CoreLocationCLI, using freegeoip - this is not accurate"
+        response_json = json.loads(requests.get(
+            'http://freegeoip.net/json/').content)
         lat = response_json['latitude']
         lng = response_json['longitude']
     else:
@@ -28,18 +28,12 @@ def main():
     # get the nearest pubs
     gmaps = googlemaps.Client(key=api_key)
     places = gmaps.places('pub|bar',
-        location={'lat': lat, 'lng': lng},
-        radius=100
-    )
+                          location={'lat': lat, 'lng': lng},
+                          radius=100
+                          )
 
     for place in places['results']:
-        directions_result = gmaps.directions({'lat': lat, 'lng': lng},
-                                     place['geometry']['location'],
-                                     mode='walking')
-                     
         print "{name} ({address})".format(name=place['name'].encode('utf8'), address=place['formatted_address'].encode('utf8'))
-        for step in directions_result[0]['legs'][0]['steps']:
-            print "\t", BeautifulSoup(step['html_instructions'], 'html.parser').get_text()
 
 
 if __name__ == '__main__':
