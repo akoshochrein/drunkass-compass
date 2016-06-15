f6b7bd197f44b965af0caff5560155808d198733
Csaba Okrona <ochronus@ochronus.com>
2016-06-15 11:21:22 +0200

    stashed commit, directions working

--- drunkass_compass/drunkass_compass.py
+++ drunkass_compass/drunkass_compass.py
@@ -3,6 +3,10 @@ import googlemaps
 import keyring
 import json
 import requests
+from datetime import datetime
+import commands
+from bs4 import BeautifulSoup
+
 
 def main():
     # get API key for google maps
@@ -12,9 +16,14 @@ def main():
         exit(1)
 
     # get current location
-    response_json = json.loads(requests.get('http://freegeoip.net/json/').content)
-    lat = response_json['latitude']
-    lng = response_json['longitude']
+    (status, output) = commands.getstatusoutput("CoreLocationCLI -once YES -format '%latitude %longitude'")
+    if status != 0:
+        print "Could not run CoreLocationCLI, using freegeoip - this is not accurate"    
+        response_json = json.loads(requests.get('http://freegeoip.net/json/').content)
+        lat = response_json['latitude']
+        lng = response_json['longitude']
+    else:
+        (lat, lng) = output.split()
 
     # get the nearest pubs
     gmaps = googlemaps.Client(key=api_key)
@@ -24,7 +33,13 @@ def main():
     )
 
     for place in places['results']:
-        print place['name'].encode('utf8')
+        directions_result = gmaps.directions({'lat': lat, 'lng': lng},
+                                     place['geometry']['location'],
+                                     mode='walking')
+                     
+        print "{name} ({address})".format(name=place['name'].encode('utf8'), address=place['formatted_address'].encode('utf8'))
+        for step in directions_result[0]['legs'][0]['steps']:
+            print "\t", BeautifulSoup(step['html_instructions'], 'html.parser').get_text()
 
 
 if __name__ == '__main__':
